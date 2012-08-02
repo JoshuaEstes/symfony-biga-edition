bc14847395f12333f81b85177cca5f15f467c0cb
Joshua Estes <Joshua.Estes@iostudio.com>
2012-08-02 14:11:17 -0500

    updated app kernel to include the framework bundle and updated composer.lock file

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new BigaFrameworkBundle\BigaFrameworkBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
